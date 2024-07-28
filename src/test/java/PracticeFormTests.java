6f63cce1241626e3b92a5c55ffc8c65f620491f4
gamelin <g-amelin@mail.ru>
2024-07-28 20:59:56 +0200

    Added additional checks and elements selects

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -11,8 +11,9 @@ public class PracticeFormTests {
 
     @BeforeAll
     static void beforeAll () {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1266x668";
+        // Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -25,6 +26,8 @@ public class PracticeFormTests {
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('.sidebar-content').remove()");
 
+        sleep(1000);
+
         // Check that opened page is exactly the page "Practice Form"
         $(".text-center").shouldHave(Condition.exactTextCaseSensitive("Practice Form"));
 
@@ -32,10 +35,23 @@ public class PracticeFormTests {
         $("#firstName").setValue("Slobodan");
         $("#lastName").setValue("Milosevic");
         $("#userEmail").setValue("slobodan@mail.ru");
-        $(byText("Male")).click();
+
+        // Universal method to select gender not looking at local language instead of $(byText("Male")).click();
+        //$("#gender-radio-1").parent().click();
+
+        $("[for=gender-radio-1]").click();
+
         //$("#gender-radio-1").
-                //selectRadio("#gender-radio-1");
+        //selectRadio("#gender-radio-1");
         $("#userNumber").setValue("7380456987");
+
+        //$("#dateOfBirth").click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOptionByValue("1980");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+
+        sleep(1000);
 
         // Choose a subject in Search drop-down menu
         $("#subjectsContainer").$("#subjectsInput").setValue("e");
@@ -46,11 +62,20 @@ public class PracticeFormTests {
         // Entering personal subject todo
         $("#subjectsContainer").$("#subjectsInput").setValue("cars");
 
+        sleep(1000);
+
         // Choose a Hobby check-box
         $("#hobbies-checkbox-1").sibling(0).click();
         $("#hobbies-checkbox-2").sibling(0).click();
         $("#hobbies-checkbox-3").sibling(0).click();
         $("#hobbies-checkbox-2").sibling(0).click();
+
+        // Upload file for Picture - Option #1
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        // Upload file for Picture - Option #2
+        $("#uploadPicture").uploadFromClasspath("img/2.jpg");
+
+        sleep(1000);
 
         // Check that personal subject doesn't exit in the list of subjects
         $("#subjectsContainer").shouldNotHave(exactTextCaseSensitive("cars")).
@@ -59,14 +84,19 @@ public class PracticeFormTests {
         // Set Current Address
         $("[placeholder='Current Address']").setValue("101000 Novi Sad, Serbija\nCara Dusana 83. sprat 243");
 
+        sleep(1000);
+
         // Select State and City
         $("#state").click();
-        $(byText("Haryana")).click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
         $("#city").click();
-        $(byText("Panipat")).click();
+        $("#stateCity-wrapper").$(byText("Panipat")).click();
+
+        sleep(1000);
 
         // Open button "Submit" and check that modal window is opened
         $("#submit").click();
+        $(".modal-dialog").should(appear);
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
 
         // Check the correctness of the data in modal window
@@ -78,7 +108,6 @@ public class PracticeFormTests {
                 .shouldHave(text("Male"));
         $(".table-responsive").$(byText("Mobile")).parent()
                 .shouldHave(text("7380456987"));
-
         $(".table-responsive").$(byText("Subjects")).parent()
                 .shouldHave(text("English, Arts"));
         $(".table-responsive").$(byText("Hobbies")).parent()
@@ -92,6 +121,6 @@ public class PracticeFormTests {
         // Close a modal window
         $("#closeLargeModal").click();
 
-        sleep(5000);
+        sleep(1000);
     }
 }
