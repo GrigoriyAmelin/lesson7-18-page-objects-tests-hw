48744f50acfdc6594941f4e6d4df668074e6d62d
gamelin <g-amelin@mail.ru>
2024-08-03 18:54:35 +0200

    Added DateBrith checks and final tuning

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.SelectRadio;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -26,8 +27,6 @@ public class PracticeFormTests {
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('.sidebar-content').remove()");
 
-        sleep(1000);
-
         // Check that opened page is exactly the page "Practice Form"
         $(".text-center").shouldHave(Condition.exactTextCaseSensitive("Practice Form"));
 
@@ -41,17 +40,14 @@ public class PracticeFormTests {
 
         $("[for=gender-radio-1]").click();
 
-        //$("#gender-radio-1").
-        //selectRadio("#gender-radio-1");
         $("#userNumber").setValue("7380456987");
 
+        // Select Date of birth
         //$("#dateOfBirth").click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOptionByValue("1980");
         $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-
-        sleep(1000);
 
         // Choose a subject in Search drop-down menu
         $("#subjectsContainer").$("#subjectsInput").setValue("e");
@@ -59,10 +55,8 @@ public class PracticeFormTests {
         $("#subjectsContainer").$("#subjectsInput").setValue("ar");
         $(byText("Arts")).click();
 
-        // Entering personal subject todo
+        // Entering personal subject
         $("#subjectsContainer").$("#subjectsInput").setValue("cars");
-
-        sleep(1000);
 
         // Choose a Hobby check-box
         $("#hobbies-checkbox-1").sibling(0).click();
@@ -75,24 +69,18 @@ public class PracticeFormTests {
         // Upload file for Picture - Option #2
         $("#uploadPicture").uploadFromClasspath("img/2.jpg");
 
-        sleep(1000);
-
         // Check that personal subject doesn't exit in the list of subjects
         $("#subjectsContainer").shouldNotHave(exactTextCaseSensitive("cars")).
                 shouldHave(text("English")).shouldHave(text("Arts"));
 
         // Set Current Address
-        $("[placeholder='Current Address']").setValue("101000 Novi Sad, Serbija\nCara Dusana 83. sprat 243");
-
-        sleep(1000);
+        $("[placeholder='Current Address']").setValue("101000 Novi Sad, Srbija\nCara Dusana 83. sprat 243");
 
         // Select State and City
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Haryana")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Panipat")).click();
-
-        sleep(1000);
 
         // Open button "Submit" and check that modal window is opened
         $("#submit").click();
@@ -108,19 +96,22 @@ public class PracticeFormTests {
                 .shouldHave(text("Male"));
         $(".table-responsive").$(byText("Mobile")).parent()
                 .shouldHave(text("7380456987"));
+        $(".table-responsive").$(byText("Date of Birth")).parent()
+                .shouldHave(text("30 April,1980"));
         $(".table-responsive").$(byText("Subjects")).parent()
                 .shouldHave(text("English, Arts"));
         $(".table-responsive").$(byText("Hobbies")).parent()
                 .shouldHave(text("Sports, Music"));
-
+        $(".table-responsive").$(byText("Hobbies")).parent()
+                .shouldHave(text("Sports, Music"));
+        $(".table-responsive").$(byText("Picture")).parent()
+                .shouldHave(text("2.jpg"));
         $(".table-responsive").$(byText("Address")).parent()
-                .shouldHave(text("101000 Novi Sad, Serbija Cara Dusana 83. sprat 243"));
+                .shouldHave(text("101000 Novi Sad, Srbija\nCara Dusana 83. sprat 243"));
         $(".table-responsive").$(byText("State and City")).parent()
                 .shouldHave(text("Haryana Panipat"));
 
         // Close a modal window
         $("#closeLargeModal").click();
-
-        sleep(1000);
     }
 }
