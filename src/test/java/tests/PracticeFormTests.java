92caebaf6b617fe02736f38efeb2adb77177a2f5
gamelin <g-amelin@mail.ru>
2024-09-15 20:51:48 +0200

    Refactor packages

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,8 +1,7 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.commands.SelectRadio;
-import org.junit.jupiter.api.BeforeAll;
+package tests;
+
 import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,8 @@
+package tests;
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import pages.RegistrationPage;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
